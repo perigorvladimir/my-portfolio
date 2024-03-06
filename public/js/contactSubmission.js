ef094d612470a78f1217df31df4a446c0f079e86
joaopedroalencar <joaopedroalencar@hotmail.com>
2024-03-06 15:35:30 -0500

    exibe mensagem enviada no contact form

--- public/js/contactSubmission.js
+++ public/js/contactSubmission.js
@@ -2,15 +2,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
     form.addEventListener("submit", function(e) {
-        e.preventDefault(); // Impede o envio padrão do formulário
+        e.preventDefault();
 
-        // Captura os valores dos campos
+        // save the values
         const firstName = document.getElementById("firstName").value;
         const email = document.getElementById("email").value;
         const message = document.getElementById("message").value;
 
         // Constrói a mensagem personalizada
-        const messageConfirmation = `Olá ${firstName}, recebemos a seguinte mensagem por você: "${message}". Retornaremos contato o mais rápido possível através do seu email ("${email}").`;
+        const messageConfirmation = `Olá ${firstName}, recebemos a seguinte mensagem por você: "${message}". Retornaremos contato o mais rápido possível através do seu email (${email}).`;
 
         // Exibe a mensagem personalizada
         document.getElementById("messageConfirmation").innerHTML = messageConfirmation;
