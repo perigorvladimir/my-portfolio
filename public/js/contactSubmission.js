531677c2c55606a5930074a294abbbbb8af851d6
joaopedroalencar <joaopedroalencar@hotmail.com>
2024-03-06 15:19:27 -0500

    contact submission js

---
+++ public/js/contactSubmission.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function(e) {
+        e.preventDefault(); // Impede o envio padrão do formulário
+
+        // Captura os valores dos campos
+        const firstName = document.getElementById("firstName").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+
+        // Constrói a mensagem personalizada
+        const messageConfirmation = `Olá ${firstName}, recebemos a seguinte mensagem por você: "${message}". Retornaremos contato o mais rápido possível através do seu email ("${email}").`;
+
+        // Exibe a mensagem personalizada
+        document.getElementById("messageConfirmation").innerHTML = messageConfirmation;
+
+        // Opcional: Limpar o formulário após o envio
+        form.reset();
+    });
+});
+
