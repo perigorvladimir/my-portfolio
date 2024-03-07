bcfbb0d26090dd72a57af43e426ae8418b11429b
joao pedro <joaopedroalencar@hotmail.com>
2024-03-06 22:07:57 -0300

    home

--- public/js/contactSubmission.js
+++ public/js/contactSubmission.js
@@ -1,21 +1,18 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
     form.addEventListener("submit", function(e) {
         e.preventDefault();
-
         // save the values
         const firstName = document.getElementById("firstName").value;
         const email = document.getElementById("email").value;
         const message = document.getElementById("message").value;
 
-       
-        const messageConfirmation = `<div>Hello ${firstName}, we have received the following message from you: "${message}". We will return contact as soon as possible through your email (${email}).</div>`;
+        const messageConfirmation = `Hello ${firstName}, we have received the following message from you: "${message}". We will return contact as soon as possible through your email (${email}).`;
 
-        
-        document.getElementById("messageConfirmation").innerHTML = messageConfirmation;
+        $('.toast').toast('show');
+        $('.toast-body').text(messageConfirmation);
 
-            form.reset();
+        form.reset();
     });
 });
 
